1b78f78c2d05dd3d9177b1e9ed8d8b024ba3413e
Cody <codyjjwbrown@gmail.com>
2015-04-01 15:33:16 -0400

    grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,32 @@
 grocery_list = ["apples", "bananas", "strawberries", "peaches"]
 
-grocery_list.each {|x|puts "* #{x}"}
+grocery_list.push("rice")
+
+def listdisplay(list)
+	puts "THE LIST"
+	list.each {|x|puts "* #{x}"}
+	puts "\n"
+end
+
+listdisplay(grocery_list)
+
+puts "The amount items you dropped is #{grocery_list.length}"
+
+if grocery_list.index("bananas")
+	puts "This shit is bananas"
+else
+	puts "This shit is not so bananas"
+end
+
+puts "Your second item was some #{grocery_list.fetch(1)}"
+
+listdisplay(grocery_list.sort)
+
+if grocery_list.index("salmon")
+	grocery_list.delete("salmon")
+	grocery_list.push("MORE salmon")
+else
+	grocery_list.push("salmon")
+end
+
+listdisplay(grocery_list)
