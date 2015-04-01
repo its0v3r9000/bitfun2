6c7387dbd85c2756a104dc097aa8766a4acebe30
Cody <codyjjwbrown@gmail.com>
2015-04-01 15:47:52 -0400

    student hash function

---
+++ exercise7.rb
@@ -0,0 +1,16 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+def listdisplay(list)
+	puts "THE Students"
+	list.each do |key, value|
+  		puts "#{key}: #{value} students"
+	end
+	puts "\n"
+end
+
+listdisplay(students)
+
